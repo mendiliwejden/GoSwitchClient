f6dd057ae47514fb1ff1c263c071e6c83a3b3bce
mendiliwejden <mendili.wejden@gmail.com>
2021-02-02 10:29:31 +0100

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './user/home/home.component'
+import { AppComponent } from './app.component';
+import { ContactComponent } from './user/contact/contact.component';
+import { ErrorComponent } from './user/error/error.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { MyHomeComponent } from './user/my-home/my-home.component';
+import { DestinationComponent } from './user/destination/destination.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+  },
+  {
+    path: "myHome",
+    component: MyHomeComponent,
+  },
+  {
+    path: "destination",
+    component: DestinationComponent,
+  },
+  {
+    path: "contact",
+    component: ContactComponent,
+  },
+  {
+    path: "**",
+    component: ErrorComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { RegisterComponent } from './user/register/register.component';
 import { LoginComponent } from './user/login/login.component';
-import { HomeComponent } from './admin/home/home.component';
+import { HomeComponent } from './user/home/home.component';
 import { ContactComponent } from './user/contact/contact.component';
 import { ErrorComponent } from './user/error/error.component';
+import { MyHomeComponent } from './user/my-home/my-home.component';
+import { DestinationComponent } from './user/destination/destination.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { ErrorComponent } from './user/error/error.component';
     LoginComponent,
     HomeComponent,
     ContactComponent,
-    ErrorComponent
+    ErrorComponent,
+    MyHomeComponent,
+    DestinationComponent
   ],
   imports: [
     BrowserModule,
