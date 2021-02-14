ff815feb7414cea7e55d2e8120ac0b1a5fcbe69d
Emna Amri <emna.amri40@gmail.com>
2021-02-14 18:21:15 +0100

    14/02/2021

---
+++ src/app/user/demande-echange/demande-echange.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demande-echange',
+  templateUrl: './demande-echange.component.html',
+  styleUrls: ['./demande-echange.component.css']
+})
+export class DemandeEchangeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
