ac8a2bdf4062035202fe1dd1591c8ab8b290d462
Emna Amri <emna.amri40@gmail.com>
2021-03-09 19:06:49 +0100

    api autentification

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +11,19 @@ export class HomeComponent implements OnInit {
 
   emailstring= "mailto:mendili.wejden@gmail.com?Subject=Hello&body=links:  %0D http://link1.com  %0D http://link1.com";
 
-  ngOnInit() {
+  content: string;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getPublicContent().subscribe(
+      data => {
+        this.content = data;
+      },
+      err => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
   }
 
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 // import custom validator to validate that password and confirm password fields match
@@ -11,40 +11,69 @@ import { MustMatch } from '../../_helpers/must-match.validator'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  
+  form: any = {};
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
   registerForm: FormGroup;
   submitted = false;
-
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-      NumTel: ['', [Validators.required, Validators.minLength(8)]],
-      firstName: ['', Validators.required],
-      region: ['', Validators.required],
-      destination: ['', Validators.required],
-      cin: ['', Validators.required],
-      steg: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required],
-      time: ['', Validators.required]
-  }, {
-      validator: MustMatch('password', 'confirmPassword')
-  });
+  mobilePattern="^((\\+91-?)|0)?[0-9]$"
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
+  ngOnInit(): void {
   }
-  // convenience getter for easy access to form fields
+
+  // ngOnInit() {
+  //   this.registerForm = this.formBuilder.group({
+  //     NumTel: ['', [Validators.required, Validators.minLength(8)]],
+  //     firstName: ['', Validators.required],
+  //     region: ['', Validators.required],
+  //     destination: ['', Validators.required],
+  //     cin: ['', Validators.required],
+  //     steg: ['', Validators.required],
+  //     lastName: ['', Validators.required],
+  //     email: ['', [Validators.required, Validators.email]],
+  //     password: ['', [Validators.required, Validators.minLength(6)]],
+  //     confirmPassword: ['', Validators.required],
+  //     time: ['', Validators.required]
+  // }, {
+  //     validator: MustMatch('password', 'confirmPassword')
+  // });
+  // }
+  // // convenience getter for easy access to form fields
   get f() 
   { return this.registerForm.controls; }
 
-  onSubmit() {
-    this.submitted = true;
-    // stop here if form is invalid
-    if (this.registerForm.invalid) {
-        return;
-    }
-    // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  // onSubmit() {
+  //   this.submitted = true;
+  //   // stop here if form is invalid
+  //   if (this.registerForm.invalid) {
+  //       return;
+  //   }
+  //   else {this.authService.register(this.registerForm).subscribe(
+  //         data => {
+  //           console.log(data);
+  //           this.isSuccessful = true;
+  //           this.isSignUpFailed = false;
+  //         },
+  //         err => {
+  //           this.errorMessage = err.error.message;
+  //           this.isSignUpFailed = true;
+  //         }
+  //       );
+  //     }
+  // }
+
+  onSubmit(): void {
+    this.authService.register(this.form).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
   }
 }
